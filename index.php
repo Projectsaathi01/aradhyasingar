59fc274710b149fa7758b83fc0b700ee59d3b4c6
projectsaathi01 <projectsaathi@gmail.com>
2020-01-14 14:17:45 +0545

    slogan added

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Rana Printing</title>
+	<title>Rana Printing | A2Z Printing Solution</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
