6433a66315e659914ae128a890a55245d4c0b9e1
projectsaathi01 <projectsaathi@gmail.com>
2020-02-16 12:36:13 +0545

    first version of aradhya

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Rana Printing | A2Z Printing Solution</title>
+	<title>Aradhya Singar & Gift Pasal</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -14,7 +14,7 @@
 	<div class="navbar navbar-expand-lg navbar-light bg-light">
 		<div class="container">
         <link rel="icon" href="assets/img/aplus.png" type="image/png">
-			<a href="#" class="navbar-brand brand-deco">Rana Printing</a>
+			<a href="#" class="navbar-brand brand-deco">Aradhya Singar & Gift Pasal</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
@@ -24,7 +24,7 @@
 							<ul class="nav navbar-nav menu_nav ml-auto">
 								<li class="nav-item active"><a class="nav-link" href="#">Home</a></li> 
 								<li class="nav-item"><a class="nav-link" href="#">About</a></li> 
-								<li class="nav-item"><a class="nav-link" href="#services">Services</a>
+								<!-- <li class="nav-item"><a class="nav-link" href="#services">Services</a> -->
 								<li class="nav-item"><a class="nav-link" href="#product">Products</a>
 								
 
@@ -41,13 +41,13 @@
 	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
 	 	 <div class="carousel-inner">
 		    <div class="carousel-item active">
-		      <img class="d-block w-100" src="assets/img/a.png" alt="First slide">
+		      <img class="d-block w-100" src="assets/img/gift.png" alt="First slide">
 		    </div>
 		    <div class="carousel-item">
-		      <img class="d-block w-100" src="assets/img/services.png" alt="Second slide">
+		      <img class="d-block w-100" src="assets/img/service.png" alt="Second slide">
 		    </div>
 		    <div class="carousel-item">
-		      <img class="d-block w-100" src="assets/img/address.png" alt="Third slide">
+		      <img class="d-block w-100" src="assets/img/contact.png" alt="Third slide">
 		    </div>
 	  </div>
 	  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -63,48 +63,6 @@
 		<img src="assets/img/img1.jpg" class="img-fluid" alt="Responsive image">
 	</section> -->
 
-	<!-- services-->
-	<div class="container service-background">
-		<div class="services_title">
-			<h3 id="services">Our Services</h3>
-			<hr>
-			
-		</div>
-
-		<div class="row">
-			
-				<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-					<div class="services-item services_deco service-border">
-						<img src="assets/img/vector.png" alt="Service First" style="max-height: 80px;">
-						<a href="#"><h4>Graphics Design</h4></a>
-						<p>We Offer Logo, Banner, ID Card, Flex & other graphics design related work.</p>
-					</div>
-				</div>
-			
-			<!-- 2nd service -->
-			
-				<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4" >
-					<div class="services-item services_deco service-border">
-						<img src="assets/img/keyboard.png" alt="Service First" style="max-height: 80px;">
-						<a href=""><h4>Content Typing</h4></a>
-						<p>We offer Content Writing & typing service in English and Nepali.</p>
-					</div>
-				</div>
-			
-			<!-- 3rd service -->
-			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-				<div class="services-item services_deco service-border">
-					<img src="assets/img/print.png" alt="Service First" style="max-height: 80px;">
-					<a href=""><h4>Printing</h4></a>
-					<p>We offer printing on Mobile Cases, Cup, Tile, T-Shirts & other garment items.</p>
-				</div>
-			</div>
-
-		</div>
-
-
-
-	</div>
 
 <!-- products -->
 	<div class="container">
@@ -117,9 +75,9 @@
 			<!-- 1st product  -->
 				<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
 					<div class="product-deco product-border">
-						<img src="assets/img/phone-case.png" alt="Service First" class="img-fluid" style="max-height: 80px;">
-						<a href=""><h4>Phone Cases</h4></a>
-						<p>We provide Phone Cases of different brands with custom graphics & also offer customer's design on cases.</p>
+						<img src="assets/img/gift.png" alt="Service First" class="img-fluid" style="max-height: 80px;">
+						<a href=""><h4>Gift Items</h4></a>
+						<p>You will get gift items with economic price</p>
 						
 				
 						<!-- modal end -->
@@ -130,9 +88,9 @@
 			<!-- 2nd product -->
 			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
 					<div class="product-deco product-border">
-						<img src="assets/img/tshirt.png" alt="Service First" class="img-fluid" style="max-height: 80px;">
-						<a href=""><h4>T-Shirts & Hood</h4></a>
-						<p>We provide T-Shirts & Hood having your own design and we have partner of Garment Productions too.</p>
+						<img src="assets/img/toy.png" alt="Service First" class="img-fluid" style="max-height: 80px;">
+						<a href=""><h4>Toys & Dolls</h4></a>
+						<p>We provide Toy for babies</p>
 					</div>
 				</div>
 				
@@ -140,9 +98,9 @@
 			<!-- 3rd product -->
 			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
 					<div class="product-deco product-border">
-						<img src="assets/img/gift.png" alt="Service First" class="img-fluid" style="max-height: 80px;">
-						<a href=""><h4>Gift Items</h4></a>
-						<p>We provide Gift Itmes like Photo Frames, Trophy, Medal & Shield and print 3D things as per customer's order.</p>
+						<img src="assets/img/parlor.png" alt="Service First" class="img-fluid" style="max-height: 80px;">
+						<a href=""><h4>Cosmetic Items</h4></a>
+						<p>We provide Cosmetic Products</p>
 
 					</div>
 			</div>
@@ -223,9 +181,9 @@
 			<div class="col-lg-6" style="color: white;">
 				<address>
 					<h3>Contact Us</h3>
-					<a mailto="aplus@gmail.com">ranaprinting.mnr@gmail.com</a>
+					<a mailto="#">aradhya_singal@gmaill.com</a>
 					<p class="postal-address"> 10400 Mahendranagar <br> Kanchanpur, Nepal </p>
-					<p>9805772487 | 9809441156</p>
+					<!-- <p>9805772487 | 9809441156</p> -->
 
 				</address>
 			</div>
@@ -244,7 +202,7 @@
 		</div>
 	</div>
 	<div class="copyright">
-		<p>Rana Printing &copy; <?php echo date("Y"); ?></p>
+		<p>Projectsaathi Team &copy; <?php echo date("Y"); ?></p>
 	</div>
 </div>
 
